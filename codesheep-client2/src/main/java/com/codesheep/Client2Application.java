21a0de9d6b27b9d2da95c0b4da2d386834a1f405
tanli <554187971@qq.com>
2019-08-04 16:49:16 +0800

    Initial commit

---
+++ codesheep-client2/src/main/java/com/codesheep/Client2Application.java
@@ -0,0 +1,11 @@
+package com.codesheep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Client2Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Client2Application.class, args);
+    }
+}
---
+++ codesheep-server/src/main/java/com/codesheep/service/SheepUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.codesheep.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.security.core.userdetails.User;
+@Component
+public class SheepUserDetailsService implements UserDetailsService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+
+        if( !"codesheep".equals(s) )
+            throw new UsernameNotFoundException("用户" + s + "不存在" );
+
+        return new User( s, passwordEncoder.encode("123456"), AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_NORMAL,ROLE_MEDIUM"));
+    }
+}
